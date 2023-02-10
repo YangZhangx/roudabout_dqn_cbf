dc064d07f4ae7b26002ceff2781eeeba7d80e95f
yangzhangx <longinusyang@gmail.com>
2023-02-10 18:03:37 +0100

    add cbf

--- dqn_agent.py
+++ dqn_agent.py
@@ -3,10 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
-
+from diff_cbf_qp import CBFQPLayer
+from utils import soft_update, hard_update
 class DeepQNetwork(nn.Module):
-    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions,env,args):
         super(DeepQNetwork, self).__init__()
+
+        self.env = env
+        self.args = args
+        
+
         self.input_dims = input_dims
         self.fc1_dims = fc1_dims
         self.fc2_dims = fc2_dims
@@ -19,6 +25,7 @@ class DeepQNetwork(nn.Module):
         self.device = T.device('cpu')
         self.to(self.device)
 
+        
     def forward(self, state):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
@@ -26,7 +33,7 @@ class DeepQNetwork(nn.Module):
         return actions
 
 class Agent():
-    def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size=15000, eps_end=0.01, eps_dec=5e-4):
+    def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size=15000, eps_end=0.01, eps_dec=5e-4, replace=1000, algo=None, env_name=None, chkpt_dir='tmp/dqn', env=None, args=None):
         self.gamma = gamma
         self.epsilon = epsilon
         self.eps_min = eps_end
@@ -37,13 +44,18 @@ class Agent():
         self.batch_size = batch_size
         self.mem_ctr = 0
 
-        self.Q_eval = DeepQNetwork(self.lr, n_actions=n_actions, input_dims=input_dims, fc1_dims=256, fc2_dims=256)
+        self.Q_eval = DeepQNetwork(self.lr, n_actions=n_actions, input_dims=input_dims, fc1_dims=256, fc2_dims=256,env=env,args=args)
 
         self.state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
         self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
         self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
         self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
+
+        # CBF layer
+        self.env = env
+        self.cbf_layer = CBFQPLayer(env, args, args.gamma_b, args.k_d, args.l_p)
+        self.diff_qp = args.diff_qp
 
     def store_transition(self, state, action, reward, state_, done):
         index = self.mem_ctr%self.mem_size
@@ -65,7 +77,7 @@ class Agent():
 
         return action
 
-    def learn(self):
+    def learn(self, memory, batch_size, updates, dynamics_model, memory_model=None, real_ratio=None):
         if self.mem_ctr < self.batch_size:
             return
 
@@ -92,3 +104,69 @@ class Agent():
         self.Q_eval.optimizer.step()
 
         self.epsilon = self.epsilon - self.eps_dec if self.epsilon > self.eps_min else self.eps_min
+
+        if memory_model and real_ratio:
+            state_batch, action_batch, reward_batch, next_state_batch, mask_batch, t_batch, next_t_batch = memory.sample(batch_size=int(real_ratio*batch_size))
+            state_batch_m, action_batch_m, reward_batch_m, next_state_batch_m, mask_batch_m, t_batch_m, next_t_batch_m = memory_model.sample(
+                batch_size=int((1-real_ratio) * batch_size))
+            state_batch = np.vstack((state_batch, state_batch_m))
+            action_batch = np.vstack((action_batch, action_batch_m))
+            reward_batch = np.hstack((reward_batch, reward_batch_m))
+            next_state_batch = np.vstack((next_state_batch, next_state_batch_m))
+            mask_batch = np.hstack((mask_batch, mask_batch_m))
+        else:
+            state_batch, action_batch, reward_batch, next_state_batch, mask_batch, t_batch, next_t_batch = memory.sample(batch_size=batch_size)
+
+        state_batch = T.FloatTensor(state_batch).to(self.device)
+        next_state_batch = T.FloatTensor(next_state_batch).to(self.device)
+        action_batch = T.FloatTensor(action_batch).to(self.device)
+        reward_batch = T.FloatTensor(reward_batch).to(self.device).unsqueeze(1)
+        mask_batch = T.FloatTensor(mask_batch).to(self.device).unsqueeze(1)
+
+        with T.no_grad():
+            next_state_action, next_state_log_pi, _ = self.policy.sample(next_state_batch)
+            if self.diff_qp:  # Compute next safe actions using Differentiable CBF-QP
+                next_state_action = self.get_safe_action(next_state_batch, next_state_action, dynamics_model)
+            qf1_next_target, qf2_next_target = self.critic_target(next_state_batch, next_state_action)
+            min_qf_next_target = T.min(qf1_next_target, qf2_next_target) - self.alpha * next_state_log_pi
+            next_q_value = reward_batch + mask_batch * self.gamma * (min_qf_next_target)
+        qf1, qf2 = self.critic(state_batch, action_batch)  # Two Q-functions to mitigate positive bias in the policy improvement step
+        qf1_loss = F.mse_loss(qf1, next_q_value)  # JQ = 𝔼(st,at)~D[0.5(Q1(st,at) - r(st,at) - γ(𝔼st+1~p[V(st+1)]))^2]
+        qf2_loss = F.mse_loss(qf2, next_q_value)  # JQ = 𝔼(st,at)~D[0.5(Q1(st,at) - r(st,at) - γ(𝔼st+1~p[V(st+1)]))^2]
+        qf_loss = qf1_loss + qf2_loss
+
+        self.critic_optim.zero_grad()
+        qf_loss.backward()
+        self.critic_optim.step()
+
+        # Compute Actions and log probabilities
+        pi, log_pi, _ = self.policy.sample(state_batch)
+        if self.diff_qp:  # Compute safe action using Differentiable CBF-QP
+            pi = self.get_safe_action(state_batch, pi, dynamics_model)
+
+        qf1_pi, qf2_pi = self.critic(state_batch, pi)
+        min_qf_pi = T.min(qf1_pi, qf2_pi)
+
+        policy_loss = ((self.alpha * log_pi) - min_qf_pi).mean() # Jπ = 𝔼st∼D,εt∼N[α * logπ(f(εt;st)|st) − Q(st,f(εt;st))]
+
+        self.policy_optim.zero_grad()
+        policy_loss.backward()
+        self.policy_optim.step()
+
+        if self.automatic_entropy_tuning:
+            alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
+
+            self.alpha_optim.zero_grad()
+            alpha_loss.backward()
+            self.alpha_optim.step()
+
+            self.alpha = self.log_alpha.exp()
+            alpha_tlogs = self.alpha.clone()  # For Comet.ml logs
+        else:
+            alpha_loss = T.tensor(0.).to(self.device)
+            alpha_tlogs =T.tensor(self.alpha)  # For Comet.ml logs
+
+        if updates % self.target_update_interval == 0:
+            soft_update(self.critic_target, self.critic, self.tau)
+
+        return qf1_loss.item(), qf2_loss.item(), policy_loss.item(), alpha_loss.item(), alpha_tlogs.item()
--- main.py
+++ main.py
@@ -4,6 +4,23 @@ import numpy as np
 import dqn_agent
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import random
+from torch.autograd import Variable
+from collections import namedtuple
+from itertools import count
+from PIL import Image
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib import animation
+from IPython.display import HTML
+from highway_env.envs.common.abstract import AbstractEnv
+from highway_env.envs.common.action import Action
+from highway_env.envs.common.observation import ObservationType
 
 config = {
 'action': {'type': 'DiscreteMetaAction'},
@@ -35,12 +52,80 @@ config = {
 'right_lane_reward': 0,
 }
 
+parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
+# Environment Args
+parser.add_argument('--env-name', default="Unicycle", help='Options is Unicycle')
+# Comet ML
+parser.add_argument('--log_comet', action='store_true', dest='log_comet', help="Whether to log data")
+parser.add_argument('--comet_key', default='', help='Comet API key')
+parser.add_argument('--comet_workspace', default='', help='Comet workspace')
+# SAC Args
+parser.add_argument('--mode', default='train', type=str, help='support option: train/test')
+parser.add_argument('--visualize', action='store_true', dest='visualize', help='visualize env -only in available test mode')
+parser.add_argument('--output', default='output', type=str, help='')
+parser.add_argument('--policy', default="Gaussian",
+                    help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
+parser.add_argument('--eval', type=bool, default=True,
+                    help='Evaluates a policy a policy every 5 episode (default: True)')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor for reward (default: 0.99)')
+parser.add_argument('--tau', type=float, default=0.005, metavar='G',
+                    help='target smoothing coefficient(τ) (default: 0.005)')
+parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
+                    help='learning rate (default: 0.0003)')
+parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
+                    help='Temperature parameter α determines the relative importance of the entropy\
+                            term against the reward (default: 0.2)')
+parser.add_argument('--automatic_entropy_tuning', type=bool, default=True, metavar='G',
+                    help='Automatically adjust α (default: False)')
+parser.add_argument('--seed', type=int, default=12345, metavar='N',
+                    help='random seed (default: 12345)')
+parser.add_argument('--batch_size', type=int, default=256, metavar='N',
+                    help='batch size (default: 256)')
+parser.add_argument('--max_episodes', type=int, default=200, metavar='N',
+                    help='maximum number of episodes (default: 200)')
+parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
+                    help='hidden size (default: 256)')
+parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
+                    help='model updates per simulator step (default: 1)')
+parser.add_argument('--start_steps', type=int, default=5000, metavar='N',
+                    help='Steps sampling random actions (default: 10000)')
+parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
+                    help='Value target update per no. of updates per step (default: 1)')
+parser.add_argument('--replay_size', type=int, default=10000000, metavar='N',
+                    help='size of replay buffer (default: 10000000)')
+parser.add_argument('--cuda', action="store_true",
+                    help='run on CUDA (default: False)')
+parser.add_argument('--device_num', type=int, default=0, help='Select GPU number for CUDA (default: 0)')
+parser.add_argument('--resume', default='default', type=str, help='Resuming model path for testing')
+parser.add_argument('--validate_episodes', default=5, type=int, help='how many episode to perform during validate experiment')
+parser.add_argument('--validate_steps', default=1000, type=int, help='how many steps to perform a validate experiment')
+# CBF, Dynamics, Env Args
+parser.add_argument('--no_diff_qp', action='store_false', dest='diff_qp', help='Should the agent diff through the CBF?')
+parser.add_argument('--gp_model_size', default=3000, type=int, help='gp')
+parser.add_argument('--gp_max_episodes', default=100, type=int, help='gp max train episodes.')
+parser.add_argument('--k_d', default=3.0, type=float)
+parser.add_argument('--gamma_b', default=20, type=float)
+parser.add_argument('--l_p', default=0.03, type=float,
+                    help="Look-ahead distance for unicycle dynamics output.")
+# Model Based Learning
+parser.add_argument('--model_based', action='store_true', dest='model_based', help='If selected, will use data from the model to train the RL agent.')
+parser.add_argument('--real_ratio', default=0.3, type=float, help='Portion of data obtained from real replay buffer for training.')
+parser.add_argument('--k_horizon', default=1, type=int, help='horizon of model-based rollouts')
+parser.add_argument('--rollout_batch_size', default=5, type=int, help='Size of initial states batch to rollout from.')
+# Compensator
+parser.add_argument('--comp_rate', default=0.005, type=float, help='Compensator learning rate')
+parser.add_argument('--comp_train_episodes', default=200, type=int, help='Number of initial episodes to train compensator for.')
+parser.add_argument('--comp_update_episode', default=50, type=int, help='Modulo for compensator updates')
+parser.add_argument('--use_comp', type=bool, default=False, help='Should the compensator be used.')
+args = parser.parse_args()
+
 env = gym.make('roundabout-v0')
 env.reset()
 env.configure(config)
 print(env.config)
 
-agent = dqn_agent.Agent(gamma=0.99, epsilon=10, batch_size=32, n_actions=env.action_space.n, eps_end=0.01, input_dims=[350], lr=5e-4)
+agent = dqn_agent.Agent(gamma=0.99, epsilon=0.01, batch_size=32, n_actions=env.action_space.n, eps_end=0.01, input_dims=[350], lr=5e-4, eps_dec=1e-5, env_name='roundabout-v0', chkpt_dir='models/')
 scores, eps_history = [], []
 n_games = 500
 for i in range(n_games):
