c2d24dec97f444fec625251b8bc38919dff61fc1
wangxu <xuwang8612@gmail.com>
2023-04-05 17:01:33 +0200

    restart

--- dqn_agent.py
+++ dqn_agent.py
@@ -3,25 +3,30 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import numpy as np
-from model import DeepQNetwork
-from compensator import Compensator
-from diff_cbf_qp import CBFQPLayer
 
-import torch
-import torch.nn.functional as F
-from torch.optim import Adam
-from utils import soft_update, hard_update
-from model import GaussianPolicy, QNetwork, DeterministicPolicy
-import numpy as np
-from diff_cbf_qp import CBFQPLayer
-from utils import to_tensor
-from compensator import Compensator
-from copy import deepcopy
+class DeepQNetwork(nn.Module):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
+        super(DeepQNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        actions = self.fc3(x)
+        return actions
 
 class Agent():
-
-    def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size=15000, eps_end=0.01, eps_dec=5e-4, env,args):
-
+    def __init__(self, gamma, epsilon, lr, input_dims, batch_size, n_actions, max_mem_size=15000, eps_end=0.01, eps_dec=5e-4):
         self.gamma = gamma
         self.epsilon = epsilon
         self.eps_min = eps_end
@@ -38,17 +43,7 @@ class Agent():
         self.new_state_memory = np.zeros((self.mem_size, *input_dims), dtype=np.float32)
         self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
         self.reward_memory = np.zeros(self.mem_size, dtype=np.float32)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=np.bool)
-        # CBF layer
-        self.env = env
-        self.cbf_layer = CBFQPLayer(env, args, gamma_b, k_d, l_p)
-        self.diff_qp = args.diff_qp
-        # compensator
-        if args.use_comp:
-            self.compensator = Compensator(num_inputs, action_space.shape[0], action_space.low, action_space.high, args)
-        else:
-            self.compensator = None
-
+        self.terminal_memory = np.zeros(self.mem_size, dtype=bool)
 
     def store_transition(self, state, action, reward, state_, done):
         index = self.mem_ctr%self.mem_size
